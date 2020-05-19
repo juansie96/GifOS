3df744d2abb86d8921fc09fd4e79ae95a3fad826
Pulga <juansie.96@gmail.com>
2020-05-19 11:39:47 -0300

    Adds search section (Structure and styles)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 document.querySelector('#dropdown-button').onclick = function (e) {
-        e.preventDefault();
-        const dropdownMenu = document.querySelector('#dropdown-menu');
-        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-    };
+    e.preventDefault();
+    const dropdownMenu = document.querySelector('#dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+};
+
+document.onclick = function(e) {
+    if (e.target === document.querySelector('body')) document.querySelector('#dropdown-menu').style.display = 'none';
+};
